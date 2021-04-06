e9ec1836a3b2d65555dea3f1068334bfe4b38c30
Isaac The Brave <53839630+Isaac-The-Brave@users.noreply.github.com>
2021-04-07 01:47:55 +0200

    Update Auto-Enum.sh

--- Auto-Enum.sh
+++ Auto-Enum.sh
@@ -1,12 +1,12 @@
 if [ $# -gt 1 ]; then
-	echo "Usage: ./script.sh <domain>"
-	echo "Example: ./script.sh yahoo.com" 
-	exit 1
+        echo "Usage: ./script.sh <domain>"
+        echo "Example: ./script.sh yahoo.com"
+        exit 1
 fi
 
 #if [ $# -gt -1 ]; then
  #       echo "Usage: ./script.sh <domain>"
-  #      echo "Example: ./script.sh yahoo.com" 
+  #      echo "Example: ./script.sh yahoo.com"
    #     exit 1
 #fi
 
@@ -20,13 +20,13 @@ fi
 TD="third-levels/";
 if [ ! -d "$TD" ]; then
     # If it doesn't create it
-    mkdir $TD 
+    mkdir $TD
 fi
 
 RES="results/";
 if [ ! -d "$RES" ]; then
     # If it doesn't create it
-    mkdir $RES 
+    mkdir $RES
 fi
 
 
@@ -37,37 +37,37 @@ python3 ~/BugBounty/Tools/Sublist3r/sublist3r.py -d $1 -o subdomains.txt
 
 
 if
-	cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$"
+        cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$"
 then
-	echo "Compiling third-level subdomains..."
-	cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u > third-level-subdomains.txt 
-	echo "Gathering fourth-level domains with Sublist3r..."
-#	for domain in $(cat third-level-subdomains.txt); do python3 ~/BugBounty/Tools/Sublist3r/sublist3r.py -d $domain -o third-levels/$domain.txt ;done
-	if [ $# -eq 2 ];
-	then
+        echo "Compiling third-level subdomains..."
+        cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u > third-level-subdomains.txt
+        echo "Gathering fourth-level domains with Sublist3r..."
+#       for domain in $(cat third-level-subdomains.txt); do python3 ~/BugBounty/Tools/Sublist3r/sublist3r.py -d $domain -o third-levels/$domain.txt ;done
+        if [ $# -eq 2 ];
+        then
         echo "Probing for alive third-levels with httprobe..."
         cat subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
-	else
-        	echo "Probing for alive third-levels with httprobe..."
-        	cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
-	fi
+        else
+                echo "Probing for alive third-levels with httprobe..."
+                cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
+        fi
 else
-	echo "No third-level domains found..."
+        echo "No third-level domains found..."
 
-		if [ $# -eq 2 ]; 
-       		then
-        	echo "Probing for alive domains with httprobe..."
-        	cat subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
-        	else
-                	echo "Probing for alive domains with httprobe..."
-                	cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
-        	fi
+                if [ $# -eq 2 ];
+                then
+                echo "Probing for alive domains with httprobe..."
+                cat subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
+                else
+                        echo "Probing for alive domains with httprobe..."
+                        cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
+                fi
 fi
 
 
 echo "Cleaning some files"
 cat third-levels/* | grep -Po "(\w+\.\w+\.\w+\.\w+)$"
-rm -rf third-level-subdomains.txt third-levels/ 
+rm -rf third-level-subdomains.txt third-levels/
 
 
 
@@ -102,7 +102,7 @@ cat spiderlinks.txt | httpx -mc 200 > spiderlinks2.txt
 cat spiderlinks2.txt > spiderlinks.txt
 rm spiderlinks2.txt
 #for webdir in $(cat spiderlinks.txt); do ffuf -w ~/BugBounty/Wordlists/common.txt -u $webdir/FUZZ -recursion -recursion-depth 3 -c -v -maxtime 60 >> dirscan/ffuf.txt;done
-echo "Making neat exploitation links with gf and some awkawk3000.." 
+echo "Making neat exploitation links with gf and some awkawk3000.."
 for patt in $(cat patterns); do gf $patt spiderlinks.txt | grep $1 | sort -u  >> interestinglinks.txt ; done
 
 awk '$0="https://"$0' probed.txt | sort -u >> interestinglinks.txt
@@ -114,9 +114,7 @@ echo "Running XSS scans on links.."
 cat interestinglinks.txt | gf xss | dalfox pipe > results/xss-results.txt
 
 echo "Running SQL Injections on links"
-# DSSS is a little slow, I'll try something else
-for sqli in $(cat interestinglinks.txt | gf sqli); do python3 ~/BugBounty/Tools/DSSS/dsss.py -u $sqli >> results/sqliresults.txt;done
-#for sqli in $(cat injectionlinks.txt); do sqlmap -u $sqli --batch >> sqliresults.txt; done
+for sqli in $(cat interestinglinks.txt | qsreplace -a | sort -u | gf sqli); do python3 ~/BugBounty/Tools/DSSS/dsss.py -u $sqli >> results/sqliresults.txt;done
 
 
 echo "Cleaning up files..."
