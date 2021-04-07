3753c29d2eb63e6b624a54945ad832e685b395ff
Isaac The Brave <53839630+Isaac-The-Brave@users.noreply.github.com>
2021-04-07 21:23:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,154 +1,134 @@
-#!/bin/bash
+if [ $# -gt 1 ]; then
+        echo "Usage: ./script.sh <domain>"
+        echo "Example: ./script.sh yahoo.com"
+        exit 1
+fi
 
-# This script is supposed to help install all the necessary tools to run an automated scan of a domain.
+#if [ $# -gt -1 ]; then
+ #       echo "Usage: ./script.sh <domain>"
+  #      echo "Example: ./script.sh yahoo.com"
+   #     exit 1
+#fi
 
-echo "updating and upgrading"
+rm -rf dirscan third-levels *.txt results
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+WS="dirscan/";
+if [ ! -d "$WS" ]; then
+    mkdir $WS
+fi
 
-clear
-# Install necessary official packages
-echo "installing necessary official packages"
+TD="third-levels/";
+if [ ! -d "$TD" ]; then
+    # If it doesn't create it
+    mkdir $TD
+fi
 
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y jq
-sudo apt-get install -y ruby-full
-sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
-sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
-sudo apt-get install -y python-setuptools
-sudo apt-get install -y libldns-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dnspython
-sudo apt-get install -y git
-sudo apt-get install -y rename
-sudo apt-get install -y xargs
-sudo apt-get install -y golang
-sudo apt-get install -y python-dnspython
+RES="results/";
+if [ ! -d "$RES" ]; then
+    # If it doesn't create it
+    mkdir $RES
+fi
 
-clear
+TL="links/";
+if [ ! -d "$TL" ]; then
+    mkdir $TL
+fi
 
-# Ready directories for downloads
+echo "Gathering subdomains with Sublist3r..."
 
-echo "Making Directories for downloads"
+python3 ~/BugBounty/Tools/Sublist3r/sublist3r.py -d $1 -o subdomains.txt
 
-mkdir ~/BugBounty
-mkdir ~/BugBounty/Tools
 
-# Start downloading and installing tools
-echo "installing and updating wpscan" 
-sudo gem install wpscan
-wpscan --update
 
-echo "wpscan installed and updated"
-echo "getting nuclei-patterns"
-git clone https://github.com/projectdiscovery/nuclei-templates.git
-echo "nuclei-patterns successfully received"
-# hakrawler - https://github.com/hakluke/hakrawler
-echo "installing hakrawler"
-go get github.com/hakluke/hakrawler
-echo "hakrawler installed"
+if
+        cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$"
+then
+        echo "Compiling third-level subdomains..."
+        cat subdomains.txt | grep -Po "(\w+\.\w+\.\w+)$" | sort -u > third-level-subdomains.txt
+        echo "Gathering fourth-level domains with Sublist3r..."
+#       for domain in $(cat third-level-subdomains.txt); do python3 ~/BugBounty/Tools/Sublist3r/sublist3r.py -d $domain -o third-levels/$domain.txt ;done
+        if [ $# -eq 2 ];
+        then
+        echo "Probing for alive third-levels with httprobe..."
+        cat subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
+        else
+                echo "Probing for alive third-levels with httprobe..."
+                cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
+        fi
+else
+        echo "No third-level domains found..."
 
-# ffuf - https://github.com/ffuf/ffuf
-echo "installing ffuf" 
+                if [ $# -eq 2 ];
+                then
+                echo "Probing for alive domains with httprobe..."
+                cat subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
+                else
+                        echo "Probing for alive domains with httprobe..."
+                        cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
+                fi
+fi
 
-go get -u github.com/ffuf/ffuf
 
-echo "ffuf installed"
+echo "Cleaning some files"
+cat third-levels/* | grep -Po "(\w+\.\w+\.\w+\.\w+)$"
+rm -rf third-level-subdomains.txt third-levels/
 
-# httprobe -https://github.com/tomnomnom/httprobe
-echo "installing httprobe"
 
-go get -u github.com/tomnomnom/httprobe
 
-echo "httprobe installed"
+echo "Running hakrawler to crawl links from live hosts"
+awk '$0="https://"$0' probed.txt | sort -u  > spiderlinks.txt
+awk '$0="http://"$0' probed.txt | sort -u  >> spiderlinks.txt
+for hak in $(cat spiderlinks.txt); do hakrawler -all -url $hak >> dirscan/hakrawler.txt;done
+cat dirscan/hakrawler.txt
+echo "Done with the first hakrawler scan."
 
-# httpx - https://github.com/projectdiscovery/httpx
-echo "installing httpx"
+cat dirscan/hakrawler.txt >> spiderlinks2.txt
+cat spiderlinks2.txt|  grep $1 | gf urls | sort -u | qsreplace -a | tr -d '*' >> spiderlinks.txt
+rm spiderlinks2.txt
+echo "Running Gospider on hakrawler links (Things start taking a while from this point onwards. Be patient.)"
 
-GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+gospider -S spiderlinks.txt -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)" --other-source >> spiderlinks2.txt
 
-echo "httpx installed"
+cat spiderlinks2.txt | grep $1 | gf urls | sort -u | tr -d '*' | qsreplace -a | >> spiderlinks.txt
+rm spiderlinks2.txt
 
-# Gospider - https://github.com/jaeles-project/gospider
-echo "installing Gospider"
 
-go get -u github.com/jaeles-project/gospider
 
-echo "Gospider installed"
+echo "Done with the first GoSpider scan!"
+echo "Running Waybackmachine on all successfully probed domain names"
+awk '$0="https://"$0' probed.txt| waybackurls | grep $1 | sort -u >> spiderlinks.txt
+awk '$0="https://"$0' probed.txt | sort -u  >> spiderlinks.txt
+echo "Waybackmachine search finished."
 
-# Waybackurls - https://github.com/tomnomnom/waybackurls
-echo "installing waybackurls"
+echo "Link crawling is now finished; find results in text file: spiderlinks.txt"
+echo "Probing all found URL's with HTTPX for all CODE 200 results."
+cat spiderlinks.txt | httpx -mc 200 > spiderlinks2.txt
+cat spiderlinks2.txt > spiderlinks.txt
+rm spiderlinks2.txt
+#for webdir in $(cat spiderlinks.txt); do ffuf -w ~/BugBounty/Wordlists/common.txt -u $webdir/FUZZ -recursion -recursion-depth 3 -c -v -maxtime 60 >> dirscan/ffuf.txt;done
+echo "Making neat exploitation links with gf and some awkawk3000.."
+for patt in $(cat patterns); do gf $patt spiderlinks.txt | grep $1 | sort -u  >> interestinglinks.txt ; done
 
-go get -u github.com/tomnomnom/waybackurls
+awk '$0="https://"$0' probed.txt | sort -u >> interestinglinks.txt
+awk '$0="http://"$0' probed.txt | sort -u  >> interestinglinks.txt
+echo "generating links to exploit"
+for patt in $(cat patterns); do gf $patt interestinglinks.txt | grep $1 | qsreplace -a | sort -u > links/$patt-links.txt;done
+echo "Running a wordpress scan on all wp-login.php links"
+cat interestinglinks.txt | gf urls | gf wordpress| grep  -F "wp-login.php" | sed 's/?.*//' > links/wp-login.txt
+for wplog in $(cat links/wp-login.txt); do wpscan -u $wplog --batch --follow-redirection --disable-tls-checks >> results/wpscanresults.txt; done
 
-echo "waybackurls installed"
-# Nuclei - https://github.com/projectdiscovery/nuclei
-echo "installing nuclei"
+echo "Running XSS scans on links.."
 
-GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+cat links/xss-links.txt | dalfox pipe > results/xss-results.txt
 
-echo "Nuclei installed"
+echo "Running SQL Injections on links"
+for sqli in $(cat links/sqli-links.txt.txt); do python3 ~/BugBounty/Tools/DSSS/dsss.py -u $sqli >> results/sqliresults.txt;done
 
-# Qsreplace - https://github.com/tomnomnom/qsreplace
-echo "installing qsreplace"
 
-go get -u github.com/tomnomnom/qsreplace
+echo "Cleaning up files!"
 
-echo "qsreplace installed"
+echo "Exploiting links with nuclei templates..."
+nuclei -t nuclei-templates/ -l interestinglinks.txt -o results/nuclei-results.txt
 
-# kxss - https://github.com/tomnomnom/hacks/tree/master/kxss
-echo "installing kxss"
 
-cd ~/go/bin/
-wget https://raw.githubusercontent.com/tomnomnom/hacks/master/kxss/main.go
-go build main.go
-rm main.go
-mv main kxss
-
-echo "kxss installed"
-
-# dsss - https://github.com/stamparm/DSSS​
-echo "installing DSSS"
-
-cd ~/BugBounty/Tools/
-git clone https://github.com/stamparm/DSSS
-
-cd ~ 
-echo "DSSS installed"
-
-# Sublist3r - https://github.com/aboul3la/Sublist3r
-echo "installing Sublist3r" 
-cd ~/BugBounty/Tools/
-git clone https://github.com/aboul3la/Sublist3r
-cd Sublist3r
-pip3 install -r requirements
-git clone https://github.com/rthalley/dnspython
-cd dnspython
-sudo python3 setup.py install
-cd ~
-# Dalfox - https://github.com/hahwul/dalfox​
-echo "Installing dalfox"
-
-GO111MODULE=on go get -v github.com/hahwul/dalfox/v2
-
-echo "dalfox installed"
-
-# Blind XSS - https://xsshunter.com
-# Requires registration (best to replace this with something more mobile)
-
-#GF-Patterns https://github.com/tomnomnom/gf
-echo "Installing gf patterns"
-go get -u github.com/tomnomnom/gf
-cd ~
-mkdir .gf
-cd .gf
-git clone https://github.com/Isaac-The-Brave/GF-Patterns-Redux
-mv GF-Patterns-Redux/*.json .
-rm -rf GF-Patterns-Redux
-echo "Installed gf patterns"
-clear
-#Installing Auto-Enum - https://github.com/Isaac-The-Brave/Auto-Enum
-echo "Installation finished. Please check ~/BugBounty and ~/Auto-Enum for future reference"
+echo "Scanning is done, please refer to results and other text files to see what I found..."
