7debb331a75de3e6b033bf6a71103614f33d33c4
Isaac The Brave <53839630+Isaac-The-Brave@users.noreply.github.com>
2021-04-12 23:52:07 +0200

    Update Auto-Enum.sh

--- Auto-Enum.sh
+++ Auto-Enum.sh
@@ -18,7 +18,7 @@ fi
    #     exit 1
 #fi
 
-rm -rf dirscan third-levels *.txt results
+rm -rf dirscan/ fourth-levels/ *.txt results/ links/ linkstemp/
 
 WS="dirscan/";
 if [ ! -d "$WS" ]; then
@@ -42,6 +42,10 @@ if [ ! -d "$TL" ]; then
     mkdir $TL
 fi
 
+LT="linkstemp/"
+if [ ! -d "$LT" ]; then
+    mkdir $LT
+fi
 
 #Recon and enumeration
 echo "Reconnaisance started:"
@@ -50,7 +54,7 @@ echo "Reconnaisance started:"
 echo "Gathering subdomains with Sublist3r..."
 
 python3 ~/BugBounty/Tools/Sublist3r/sublist3r.py -d $1 -o subdomains.txt
-echo $1 > subdomains.txt
+echo $1 >> subdomains.txt
 # In case sublist3r finds nothing, it's important to at least have the tried domain in the subdomain list, this is why I use this command.
 
 
@@ -65,7 +69,7 @@ then
         if [ $# -eq 2 ];
         then
         echo "Probing for alive third-levels with httprobe..."
-        cat subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
+        cat third-level-subdomains.txt | sort -u | grep -v $2 | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > probed.txt
         else
                 echo "Probing for alive third-levels with httprobe..."
                 cat subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///'  | tr -d ":443" > probed.txt
@@ -85,27 +89,25 @@ fi
 
 #Looks for fourth level subdomains and shoves them into our subdomains file.
 echo "Cleaning some files"
-cat third-levels/* | grep -Po "(\w+\.\w+\.\w+\.\w+)$" >> subdomains.txt
-rm -rf third-level-subdomains.txt fourth-levels/
+cat fourth-levels/*.txt
 echo "Shuffling files"
 #Adds http and https to the successfully probed domains to crawl. I use HTTP because not everyone is HTTPS Compliant and I wouldn't want to miss those. (Could possible double the scan time. Watch out)
-awk '$0="https://"$0' probed.txt | sort -u  > spiderlinks.txt
+awk '$0="https://"$0' probed.txt | sort -u >> spiderlinks.txt
 awk '$0="http://"$0' probed.txt | sort -u  >> spiderlinks.txt
 
 
 #Runs Waybackurls to find old links (Some of them are no longer visible on google, some lucky break might occur)
 echo "Running Waybackmachine on all successfully probed domain names"
-awk '$0="https://"$0' probed.txt| waybackurls | grep $1 | qsreplace 'input' | sort -u | httpx -mc 200 >> spiderlinks.txt
-cat spiderlinks.txt | waybackurls | gf urls| qsreplace 'input' | sort -u | gf urls | tee spiderlinks2.txt
+awk '$0="https://"$0' probed.txt | waybackurls | grep $1 | qsreplace 'input' | sort -u  >> waybackurls.txt
 echo "Waybackmachine search finished."
 
 #Runs Gospider on all picked up domains to find any links assosciated with them, cleans them up into URL's within our scope and moves them to the next step (EXPLOITATION!)
 
 echo "Running Gospider on domains (Things start taking a while from this point onwards. Be patient.)"
 
-gospider -S spiderlinks.txt >> spiderlinks2.txt
+gospider -S spiderlinks.txt -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)" --other-source > spiderlinks2.txt
 
-cat spiderlinks2.txt | grep $1 | gf urls | sort -u | tr -d '*' | qsreplace 'input' >> spiderlinks.txt
+cat spiderlinks2.txt | gf urls | grep $1 | sort -u | qsreplace 'input' >> spiderlinks.txt
 rm spiderlinks2.txt
 echo "Done with the GoSpider scan!"
 echo "Link crawling is now finished; find results in text file: spiderlinks.txt"
@@ -113,11 +115,10 @@ echo "Link crawling is now finished; find results in text file: spiderlinks.txt"
 #Uses gf to find possible injection points. (GF Patterns can be independently modified and I recommend you do so, a lot of parameters can go unnoticed with many of the patterns on github)
 
 echo "Making neat exploitation links with gf"
-awk '$0="https://"$0' probed.txt | sort -u >> interestinglinks.txt
-awk '$0="http://"$0' probed.txt | sort -u  >> interestinglinks.txt
 echo "generating links to exploit"
-for patt in $(cat patterns); do gf $patt spiderlinks.txt | grep $1 | qsreplace 'input' | sort -u >  links/$patt-links.txt;done
-
+for patt in $(cat patterns); do gf $patt spiderlinks.txt | grep $1 | qsreplace 'input' | sort -u | tee linkstemp/$patt-links.txt;done
+for patt in $(cat patterns); do cat linkstemp/$patt-links.txt | gf $patt | httpx -mc 200 > links/$patt-links.txt;done
+rm -rf linkstemp/
 clear
 
 # Uses fimap to search for Local File Inclusion vulnerabilities
@@ -141,7 +142,7 @@ echo "Cleaning up files!"
 echo "Exploiting links with nuclei templates..."
 nuclei -t nuclei-templates/ -l interestinglinks.txt -o results/nuclei-results.txt
 
+echo "Checking for valid waybackurls"
+httpx -mc 200 -l waybackurls.txt > spiderlinks.txt
 
 echo "Scanning is done, please refer to results and other text files to see what I found..."
-echo "Successful SQL Injections:"
-cat results/sqli-results.txt | grep
